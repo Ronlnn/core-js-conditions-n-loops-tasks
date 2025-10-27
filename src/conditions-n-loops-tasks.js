f3b30a9d0b3b09ffcc33da6b3fd14ec77064eb02
ronlnn <artking01kg@gmail.com>
2025-10-27 13:57:03 +0300

    feat: getMaxNumber function is done

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -44,8 +44,19 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let max = 0;
+  if (a > b && a > c) {
+    max = a;
+  } else if (b > a && b > c) {
+    max = b;
+  } else {
+    max = c;
+  }
+  if (!a || !b || !c) {
+    throw new Error('Not implemented');
+  }
+  return max;
 }
 
 /**
